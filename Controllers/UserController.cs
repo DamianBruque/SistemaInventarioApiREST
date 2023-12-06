3ff5863a9aa4d24870e8047e04d70cadeb3e6a09
Damian Bruque <dbruque@bdtglobal.com>
2023-12-06 17:55:17 -0300

    Controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,16 @@
 ﻿
+using Services;
+
 namespace Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class UserController
     {
+        private readonly UserService service;
+        public UserController(UserService userService)
+        {
+            service = userService;
+        }
     }
 }
--- Models/Entities/UserEntity.cs
+++ Models/Entities/UserEntity.cs
@@ -13,6 +13,11 @@ namespace Models.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string Password { get; set; }
     }
 }

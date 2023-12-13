e7c35fe834cc8b77298386aca7c815898fbe2259
DaamiAle <daamiale.recoil@gmail.com>
2023-12-13 09:24:45 -0300

    Security base

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,20 +6,13 @@ using Models.DAO;
 namespace Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class UserController
+    [Route("/user")]
+    public class UserController : ControllerBase
     {
         private readonly UserService service;
         public UserController(UserService userService)
         {
             service = userService;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<UserDAO>>> GetAll()
-        {
-            List<UserDAO> users = await service.GetUsers();
-            return users;
         }
     }
 }

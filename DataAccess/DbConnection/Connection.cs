7030f69d20e5621e979a40eb38b17a08e9baafd2
Mariel Marquez <75107742+MarielMarquez@users.noreply.github.com>
2023-12-18 17:50:14 -0300

    OracleConnection

---
+++ DataAccess/DbConnection/Connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbConnection
+{
+    public class Connection<T> : IConnection<T> where T : class
+    {
+        private readonly T _connection;
+        public Connection(T connection)
+        {
+            _connection = connection;
+        }
+        public T GetConnectionAccess()
+        {
+            return _connection;
+        }
+    }
+}
